87a356501ca65824ca3c3b9d1c07e7ebd8558c6a
Steven De Coeyer <steven@banteng.be>
2014-02-05 21:37:39 +0100

    Adds ChefZero tests

---
+++ test/chef_zero_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class ChefZeroTest < MiniTest::Unit::TestCase
+  include Bailiff::ChefZero
+
+  def test_start_chef_server
+    skip 'chef server starts'
+  end
+
+  def test_upload_to_chef_server
+    skip 'uploads to chef server'
+  end
+end
