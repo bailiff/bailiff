b904bb4cff4c6948357af89135d83c56a099f006
Jeroen De Vos & Steven De Coeyer <pair@openminds.be>
2014-02-06 11:52:33 +0100

    Updates ChefZeroTest

--- test/chef_zero_test.rb
+++ test/chef_zero_test.rb
@@ -3,11 +3,23 @@ require 'test_helper'
 class ChefZeroTest < MiniTest::Unit::TestCase
   include Bailiff::ChefZero
 
+  def server
+    @server ||= ::ChefZero::Server.new port: @@port
+  end
+
   def test_start_chef_server
-    skip 'chef server starts'
+    server.stub :start_background, true do
+      assert start_chef_server server
+    end
+  end
+
+  def test_refute_start_chef_server
+    server.stub :start_background, false do
+      refute start_chef_server server
+    end
   end
 
   def test_upload_to_chef_server
-    skip 'uploads to chef server'
+    skip 'This should be tested when this method is actually called.'
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,7 @@
 require 'bailiff'
 require 'minitest/unit'
 require 'minitest/autorun'
+
+# configuration expected from environment variables
+@@port = 4242
+@@chef_repo_path = '/tmp/'
