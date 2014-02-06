c726212ecd638dfdaee68fc570a8927b3c526f2d
Steven De Coeyer <steven@banteng.be>
2014-02-06 15:39:51 +0100

    Makes ChefZero methods more self-explanatory

--- lib/bailiff/chef_zero.rb
+++ lib/bailiff/chef_zero.rb
@@ -2,15 +2,15 @@ require 'chef_zero/server'
 
 module Bailiff
   module ChefZero
-    def create_chef_server
+    def chef_zero_server
       ::ChefZero::Server.new(port: ENV['CHEF_ZERO_PORT'])
     end
 
-    def start_chef_server(server = create_chef_server)
+    def start_chef_zero_server(server = chef_zero_server)
       server.start_background
     end
 
-    def upload_to_chef_server
+    def upload_repo_to_chef_zero_server
       Dir.chdir ENV['CHEF_REPO_PATH']
       %x[knife upload . -c #{ENV['KNIFE_CONFIG_PATH']}]
     end
--- test/chef_zero_test.rb
+++ test/chef_zero_test.rb
@@ -7,19 +7,19 @@ class ChefZeroTest < MiniTest::Unit::TestCase
     @server ||= ::ChefZero::Server.new port: @@port
   end
 
-  def test_start_chef_server
+  def test_start_chef_zero_server
     server.stub :start_background, true do
-      assert start_chef_server server
+      assert start_chef_zero_server server
     end
   end
 
-  def test_refute_start_chef_server
+  def test_refute_start_chef_zero_server
     server.stub :start_background, false do
-      refute start_chef_server server
+      refute start_chef_zero_server server
     end
   end
 
-  def test_upload_to_chef_server
+  def test_upload_repo_to_chef_zero_server
     skip 'This should be tested when this method is actually called.'
   end
 end
